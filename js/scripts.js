d5c9d7529384e871ca8c7e55286d3ee40c511afe
Robin <nicodemusnepomuk@gmail.com>
2019-09-09 23:37:56 +0200

    Update scripts.js
    loadListRepeater() still only workaround
    
    added and working:
    searchfunction that updates the displayed data( updateList() )
    
    rewriting of the other functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,35 @@
 //rewritten pokedex code comes here
-var deadRepository = (function($) {
+var dR = (function($) {
   //immediate stuff here
   var repository = [];
   var apiUrl = 'https://rickandmortyapi.com/api/character/?status=dead';
+  var urlInfo = {pages: 1, next: apiUrl};
+
+  //filter the repository by checking whether a given string in lowercase is part ...
+  //of the value (in lowercase) of the name property of objects in the repository.
+  //returns an array of objects whose name value correspond to the given string or an empty one
+  function filterByName(inputNameString) {
+  	return repository.filter(function (repositoryObj){
+  		return repositoryObj.name.toLowerCase().indexOf(inputNameString.toLowerCase()) != -1;
+  	})
+  }
+
+  function updateList(inputNameString)  {
+    //clear list
+    $('#data-list').empty();
+
+    var filteredData = dR.filterByName(inputNameString);
+    //add new results
+    $.each(filteredData, function(i,obj ){
+      dR.addListItem(obj);
+    });
+
+  }
+
+  function updateUrl(info){
+    urlInfo.pages = info.pages;
+    urlInfo.next = info.next;
+  }
 
   function add(mortuus){
     if (typeof(mortuus) === 'object') {
@@ -21,58 +48,210 @@ var deadRepository = (function($) {
     $('#data-list').append('<li class="mortuus-list__item"><button class="list-item__button"></button></li>');
     //set button text and functionality:
     $('.list-item__button').last()
-      .text(mortuusObj.name);
+      .text(mortuusObj.name)
       .click(function(event){
         showDetails(mortuusObj);
       });
   }
 
+//don´t forget to iterate through the other pages as well
   function loadList() {
-    $.ajax(apiUrl, {
-      method: 'GET',
-      dataType: 'json',
-      timeout: 4000
-    }).then(function(response){
-      console.log(resonse);
-    }).catch(function(err){
-      console.log('Sorry! Error: '+ err.statusText)
-    });
+    var d = $.Deferred();
+      $.ajax(urlInfo.next, {
+        method: 'GET',
+        dataType: 'json',
+        timeout: 4000
+      }).then(function(response){
+          console.log(response)
+          //update the url
+          var meta = response.info;
+          updateUrl(meta);
+
+          //iterate through the response:
+          var results = response.results;
+          $.each(results, function(i){
+            var deadObj = {
+              name: results[i].name,
+              detailsUrl: results[i].url
+            };
+            add(deadObj);
+            d.resolve();
+          });
+        }).catch(function(err){
+          console.log('Sorry! Error: '+ err.statusText)
+          d.reject();
+      });
+      return d.promise();
   }
 
-  function loadDetails() {
 
+//THIS IS A TEMPORARY AND BAD WORKAROUND
+  function loadListRepeater() {
+    var d = $.Deferred();
+      setTimeout(function() {
+          loadList();
+      }, 2000);
+      setTimeout(function() {
+          loadList();
+      }, 4000);
+      setTimeout(function() {
+          loadList();
+      }, 6000);
+      setTimeout(function() {
+          loadList();
+      },8000);
+      setTimeout(function() {
+          loadList();
+      }, 10000);
+      setTimeout(function() {
+          loadList();
+          d.resolve();
+      }, 12000);
+      setTimeout(function() {
+          d.resolve();
+      }, 13000);
+     return d.promise();
+  };
+  // function loadListRepeater() {
+  //   var d = $.Deferred();
+  //
+  //   loadList().then( function(){
+  //     var pages = urlInfo[0].pages;
+  //
+  //     async function loop() {
+  //       for (i = 1; i < pages; i++) {
+  //         await loadList();
+  //       }
+  //     }
+  //   });
+  //   d.resolve();
+  //   return d.promise();
+  // };
+
+
+  function loadDetails(repositoryObject) {
+    var d = $.Deferred();
+    var url = repositoryObject.detailsUrl;
+
+    $.ajax(url, {
+      method: 'GET',
+      dataType: 'json',
+      timeout: 2000
+    }).then( function(response) {
+      repositoryObject.species = response.species;
+      repositoryObject.gender = response.gender;
+      repositoryObject.origin = response.origin.name;
+      repositoryObject.location = response.location.name;
+      repositoryObject.imageUrl = response.image;
+      //success
+      d.resolve();
+    }).catch( function(err) {
+      //failure
+      console.log('Sorry! Error: '+ err.statusText);
+      d.reject();
+    })
+
+    return d.promise();
   }
 
   function showDetails(mortuusObj) {
     loadDetails(mortuusObj).then(function() {
 			console.log(mortuusObj);
-			showModal(mortuusObj);
+		  showModal(mortuusObj);
+    })
   }
 
+  //1. create a modal, 2. append children incl classes,
+  // 3. add fixed content, 4. add flexible content, 5. show it all
   function showModal(mortuusObj) {
+    // 1
     $('#modal-container')
       .empty()
-      .append('<div class="modal"></div>')
-      .children('modal')
-          .append('<h2 class="modal-title"></h2>')
-          .append('<img class="modal-img">')
-          .append('<p></p>')
+      .append('<div class="modal"></div>');
+    //2
+    $('.modal')
+        .append('<button class="modal-close">close<button>')
+        .append('<h2 class="modal-title"></h2>')
+        .append('<img class="modal-img">')
+        .append('<p class="modal-birth"></p>')
+        .append('<p class="modal-death"></p>')
+        .append('<p class="modal-species"></p>');
+    //3
+    $('.modal-close').click(function() {hideModal();});
+    $('.modal-title').text(mortuusObj.name);
+    $('.modal-img').attr('src', mortuusObj.imageUrl);
+    //4
+    $('.modal-birth').text(getBirthText(mortuusObj));
+    $('.modal-death').text(getDeathText(mortuusObj));
+    $('.modal-species').text(getSpeciesText(mortuusObj));
+    //5
+    $('#modal-container').addClass('is-visible');
   }
+  //modal EventListeners:
+  $('#modal-container').click(function() {hideModal();});
+  $(document).keydown(function (e) {
+    if (e.key === "Escape") {
+      hideModal();
+    }
+  })
 
   function hideModal() {
-
+    $('#modal-container').removeClass('is-visible');
   }
-  //two EventListeners here in original
+
+  // modal-output modification:
+  function getPossesivePronoun(mortuusObj) {
+    if (mortuusObj.gender === "Female") {
+      return "Her";
+    } else if (mortuusObj.gender === "Male") {
+      return "His";
+    } else {
+      return mortuusObj.name + '´s';
+    }
+  }
+
+  function getBirthText(mortuusObj) {
+    if (mortuusObj.origin === "unknown") {
+      return mortuusObj.name + '´s birthplace is not known.';
+    }
+    return mortuusObj.name + ' recieved the gift of life on ' + mortuusObj.origin + '.';
+  }
+
+  function getDeathText(mortuusObj) {
+    if (mortuusObj.location === "unknown") {
+      return mortuusObj.name + ' went missing.';
+    }
+    return getPossesivePronoun(mortuusObj) + ' final place of rest is ' + mortuusObj.location + '.';
+  }
+
+  function getSpeciesText(mortuusObj) {
+    if (mortuusObj.location === "unknown") {
+      return getPossesivePronoun(mortuusObj)+ ' family values the time they had together.';
+    }
+    return getPossesivePronoun(mortuusObj)+ ' ' + mortuusObj.species +' family values the time they had together.';
+  }
+  //modal modification end
 
   $(document).ready(function(){
   //here comes stuff that depends on the DOM
+    dR.loadList().then(function(){
+      var repo = dR.getAll();
+      $.each(repo, function(i,obj ){
+        dR.addListItem(obj);
+      });
+    });
+  //end $(document).ready :
   });
 
   return {
-    add: add,
-    getAll: getAll,
+    updateList: updateList,
+    filterByName: filterByName,
+    showModal: showModal,
+    updateUrl: updateUrl,
+    showDetails: showDetails,
     addListItem: addListItem,
+    getAll: getAll,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadListRepeater: loadListRepeater
   }
 })(jQuery);
